2972b42ea608922ce53e36cfd370168189804b4b
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-20 21:14:23 -0300

    buildconfig {buildSrc} created buildSrc for dependency centralization

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,24 @@
+object Libs {
+    const val navigationFragment =
+        "androidx.navigation:navigation-fragment-ktx:" + Versions.navUi
+
+    //Core
+    const val androidxCore =
+        "androidx.core:core-ktx:" + Versions.androidxCore
+    const val androidxAppCompat =
+        "androidx.appcompat:appcompat:" + Versions.androidxAppCompat
+    const val googleMaterial =
+        "com.google.android.material:material:" + Versions.googleMaterial
+    const val androidConstraintLayout =
+        "androidx.constraintlayout:constraintlayout:" + Versions.constraintLayout
+
+    //Tests
+    const val junit =
+        "junit:junit:" + Versions.junit
+    const val androidxTestExtJunit =
+        "androidx.test.ext:junit:" + Versions.androidxTestExtJunit
+    const val androidxEspresso =
+        "androidx.test.espresso:espresso-core:" + Versions.androidxEspresso
+}
+
+
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val navUi = "2.3.5"
+    const val androidxCore = "1.6.0"
+    const val androidxAppCompat = "1.3.1"
+    const val googleMaterial = "1.4.0"
+    const val constraintLayout = "2.0.4"
+
+    //Tests
+    const val junit = "4.13.2"
+    const val androidxTestExtJunit = "1.1.3"
+    const val androidxEspresso = "3.4.0"
+}
+
