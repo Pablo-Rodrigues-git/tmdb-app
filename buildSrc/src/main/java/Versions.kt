7b1b28d400bd33d766819a5abee285460a1b4d1c
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-20 21:43:21 -0300

    buildconfig {buildSrc} created android.gradle to centralize core dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,5 +9,13 @@ object Versions {
     const val junit = "4.13.2"
     const val androidxTestExtJunit = "1.1.3"
     const val androidxEspresso = "3.4.0"
+
+    //App Versions
+    const val compileSdk = 32
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+
 }
 
