5fee50656a03d88f2bc205bc5d938420ef96a8e5
Pablo <pablorodrigues@mobills.com.br>
2022-08-09 21:16:31 -0300

    created nav_graph, and first navigation layout(list), created bottomNavigation for MainActivity.kt

--- app/src/main/java/com/pablo/study/tmdb_app/MainActivity.kt
+++ app/src/main/java/com/pablo/study/tmdb_app/MainActivity.kt
@@ -2,10 +2,31 @@ package com.pablo.study.tmdb_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.pablo.study.tmdb_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFrament: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView(binding)
+    }
+
+    private fun initView(binding: ActivityMainBinding) {
+        navHostFrament =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFrament.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
     }
 }
