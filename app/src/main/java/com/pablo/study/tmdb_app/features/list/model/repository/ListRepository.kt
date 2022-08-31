f754bd9f443a1d5aadfac8af32b0dc6f0d4aaa01
Pablo <pablorodrigues@mobills.com.br>
2022-08-30 22:14:44 -0300

    creating remote data source and api response

--- app/src/main/java/com/pablo/study/tmdb_app/features/list/model/repository/ListRepository.kt
+++ app/src/main/java/com/pablo/study/tmdb_app/features/list/model/repository/ListRepository.kt
@@ -1,8 +1,8 @@
 package com.pablo.study.tmdb_app.features.list.model.repository
 
-import com.pablo.study.tmdb_app.data.model.MovieResponse
+import com.pablo.study.tmdb_app.data.model.ListResponse
 import com.pablo.study.tmdb_app.utils.Resource
 
 interface ListRepository {
-    suspend fun getAllMovies() : Resource <List<MovieResponse>>
+    suspend fun getAllMovies() : Resource <ListResponse>
 }
