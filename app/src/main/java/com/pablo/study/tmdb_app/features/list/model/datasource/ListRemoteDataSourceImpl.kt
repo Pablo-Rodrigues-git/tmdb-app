3b9811ded0f2a61ba2aac581382c05bdccd65203
Pablo <pablorodrigues@mobills.com.br>
2022-08-30 22:13:57 -0300

    creating remote data source and api response

--- app/src/main/java/com/pablo/study/tmdb_app/features/list/model/datasource/ListRemoteDataSourceImpl.kt
+++ app/src/main/java/com/pablo/study/tmdb_app/features/list/model/datasource/ListRemoteDataSourceImpl.kt
@@ -1,11 +1,11 @@
 package com.pablo.study.tmdb_app.features.list.model.datasource
 
-import com.pablo.study.tmdb_app.data.model.MovieResponse
+import com.pablo.study.tmdb_app.data.model.ListResponse
 import com.pablo.study.tmdb_app.data.remote.RetrofitService
 import retrofit2.Response
 
 class ListRemoteDataSourceImpl(
     private val service: RetrofitService
 ) : ListRemoteDataSource {
-    override suspend fun getAllMovies(): Response<List<MovieResponse>> = service.getAllMovies()
+    override suspend fun getAllMovies(): Response<ListResponse> = service.getAllMovies()
 }
