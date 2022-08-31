3c5133f62d1b96e0ca5f6c01e52266fb7df3da0e
Pablo <pablorodrigues@mobills.com.br>
2022-08-30 22:14:51 -0300

    creating remote data source and api response

--- app/src/main/java/com/pablo/study/tmdb_app/features/list/model/repository/ListRepositoryImpl.kt
+++ app/src/main/java/com/pablo/study/tmdb_app/features/list/model/repository/ListRepositoryImpl.kt
@@ -1,13 +1,13 @@
 package com.pablo.study.tmdb_app.features.list.model.repository
 
-import com.pablo.study.tmdb_app.data.model.MovieResponse
+import com.pablo.study.tmdb_app.data.model.ListResponse
 import com.pablo.study.tmdb_app.features.list.model.datasource.ListRemoteDataSource
 import com.pablo.study.tmdb_app.utils.Resource
 
 class ListRepositoryImpl(
     private val remoteDataSource: ListRemoteDataSource
 ) : ListRepository {
-    override suspend fun getAllMovies(): Resource<List<MovieResponse>> {
+    override suspend fun getAllMovies(): Resource<ListResponse> {
         val response = remoteDataSource.getAllMovies()
         return if (response.isSuccessful) {
             val responseBody = response.body()
