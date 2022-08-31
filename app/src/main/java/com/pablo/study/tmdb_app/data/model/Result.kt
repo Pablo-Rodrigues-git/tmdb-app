0a4968d6b21609394e4ff90e5eeec209beab53e1
Pablo <pablorodrigues@mobills.com.br>
2022-08-30 22:15:44 -0300

    creating remote data source and api response

---
+++ app/src/main/java/com/pablo/study/tmdb_app/data/model/Result.kt
@@ -0,0 +1,13 @@
+package com.pablo.study.tmdb_app.data.model
+
+data class Result(
+    val id: Int,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val vote_average: Double,
+    val vote_count: Int
+)
--- app/src/main/java/com/pablo/study/tmdb_app/data/remote/RetrofitService.kt
+++ app/src/main/java/com/pablo/study/tmdb_app/data/remote/RetrofitService.kt
@@ -1,11 +1,16 @@
 package com.pablo.study.tmdb_app.data.remote
 
-import com.pablo.study.tmdb_app.data.model.MovieResponse
+import com.pablo.study.tmdb_app.data.model.ListResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitService {
-    @GET("")
-    suspend fun getAllMovies(): Response<List<MovieResponse>>
+    @GET("/3/movie/popular")
+    suspend fun getAllMovies(
+        @Query("api_key") apiKey: String = "6fc65751e19034b021256a5c7960a780",
+        @Query("language") language: String = "en-US",
+        @Query("page") page: String = "1"
+    ): Response<ListResponse>
 }
 
